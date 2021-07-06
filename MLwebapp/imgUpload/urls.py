7165200f2a871883708a75726ddb9dee6c776534
utkarshamokashi <65499431+utkarshamokashi@users.noreply.github.com>
2021-07-06 20:28:18 +0530

    Delete MLwebapp directory

--- MLwebapp/imgUpload/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include, re_path
-from . import views
-urlpatterns = [
-    re_path(r'^$', views.home, name='home'),
-    re_path(r'imageprocess', views.imageprocess, name="imageprocess")
-]
